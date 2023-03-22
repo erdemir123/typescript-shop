b6b610da36a8ca342eedcf8d73d7831054e7c581
erdemir123 <sadikerdemir93@gmail.com>
2023-03-23 00:53:38 +0300

    home designed

--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -1,14 +1,39 @@
-import React from 'react'
-import { ProductType } from '../types/ProductTpye'
-interface ıProps{
-    product:ProductType
+import React from "react";
+import { FaTruck } from "react-icons/fa";
+import { RiHeartAddLine } from "react-icons/ri";
+import { ProductType } from "../types/ProductTpye";
+interface ıProps {
+  product: ProductType;
 }
 
-const Card = ({product}:ıProps) => {
-    console.log(product)
+const Card = ({ product }: ıProps) => {
+  console.log(product);
   return (
-    <div>Card</div>
-  )
-}
+    <div>
+      <div className="bg-orange-400 flex flex-col justify-center items-center w-[300px] h-[400px] border-2 border-orange-800 mx-auto my-4 relative shadow-md shadow-orange-900 rounded-md">
+        <span className="absolute top-2 left-2 text-slate-500 font-bold text-md flex items-center gap-2">
+          Kargo Bedava <FaTruck />
+        </span>
+        <RiHeartAddLine className="absolute top-[9px] right-4 text-xl text-red-900" />
+        <img src={product.url} alt="" className="w-48" />
+        <p className="my-2 text-2xl font-bold text-slate-600">
+          {product.price} TL
+        </p>
+        <p className="text-center my-2 text-md font-bold text-slate-600">
+          {product.name}
+        </p>
+        <div className="absolute bottom-5 flex gap-6">
+          <button className="py-1 px-2 bg-slate-400 rounded-lg shadow-sm shadow-slate-200  text-slate-800  font-bold text-md active:scale-95">
+            İncele
+          </button>
+          <span className="py-1 px-6 bg-slate-400 rounded-lg shadow-sm shadow-slate-200  text-slate-800  font-bold text-md active:scale-95"></span>
+          <button className="py-1 px-2 bg-slate-400 rounded-lg shadow-sm shadow-slate-200 text-slate-800  font-bold text-md active:scale-95">
+            Sepete ekle
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
